b9ca8700e9468c50e308aabbd4e95436e3310059
Dallas Washburn <soldbydw@gmail.com>
2020-04-28 05:49:37 -0700

    added javascript

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Read-Me-Generator
+
+                 # I wanna sleep
+                          
+                          ## Description
+                          
+                          Honestly dont know
+                          
+                          ## Table of Contents
+                          
+                          * [Installation](#installation)
+                          * [Usage](#usage)
+                          * [License](#license)
+                          * [Contributing](#contributing)
+                          * [Tests](#tests)
+                          * [Questions](#questions)
+                          
+                          ## Installation
+                          
+                          To install necassary dependencies, run the following command:
+                          
+                          no idea
+                          
+                          ## Usage
+                          
+                          her
+                          
+                          ## Contributing
+                          
+                          grrr
+                          
+                          ## Tests 
+                          
+                          To run test, run the following command:
+                          
+                          tests
+                          
+                          ## Questions
+                          
+                          ![GitHub Avatar]()
+            
+            If you have any questions about the repo, open an issue or contact  directly
+            
+                          
---
+++ index.js
@@ -0,0 +1,159 @@
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require("fs");
+const util = require("util");
+let avatar = ""
+let githubUrl = ""
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+    return inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "username",
+                message: "What is your GitHub username?"
+            },
+            {
+                type: "input",
+                name: "project",
+                message: "What is your project name?"
+            },
+            {
+                type: "input",
+                name: "desc",
+                message: "Please wright a short description of your project"
+            },
+            {
+                type: "input",
+                name: "license",
+                message: "What kind of license should your project have?"
+            },
+            {
+                type: "input",
+                name: "install",
+                message: "What command should be run to install dependencies"
+            },
+            {
+                type: "input",
+                name: "tests",
+                message: "What command should be run to run tests?"
+            },
+            {
+                type: "input",
+                name: "needToKnow",
+                message: "What does the user need to know about using the repo?"
+            },
+            {
+                type: "input",
+                name: "contribute",
+                message: "What does the user need to know about contributing to the repo?"
+            }
+        ])
+}
+
+function getGit(answers) {
+    const queryUrl = `https://api.github.com/users/${answers.username}`;
+
+    return axios.get(queryUrl)
+        .then((response) => {
+            //   console.log(response);
+            let avatar = response.data.avatar_url;
+            let githubUrl = response.data.url
+            console.log(avatar);
+            console.log(githubUrl);
+            
+            
+        })
+}
+
+
+function generateReadMe(answers) {
+    return `
+                 # ${answers.project}
+                          
+                          ## Description
+                          
+                          ${answers.desc}
+                          
+                          ## Table of Contents
+                          
+                          * [Installation](#installation)
+                          * [Usage](#usage)
+                          * [License](#license)
+                          * [Contributing](#contributing)
+                          * [Tests](#tests)
+                          * [Questions](#questions)
+                          
+                          ## Installation
+                          
+                          To install necassary dependencies, run the following command:
+                          
+                          ${answers.install}
+                          
+                          ## Usage
+                          
+                          ${answers.needToKnow}
+                          
+                          ## Contributing
+                          
+                          ${answers.contribute}
+                          
+                          ## Tests 
+                          
+                          To run test, run the following command:
+                          
+                          ${answers.tests}
+                          
+                          ## Questions
+                          
+                          ![GitHub Avatar](${avatar})
+            
+            If you have any questions about the repo, open an issue or contact ${githubUrl} directly
+            
+                          `;
+}
+
+
+promptUser()
+    // .then((answers) => {
+    //     return getGit(answers);
+        
+    // })
+    .then((answers) => {
+        const md = generateReadMe(answers);
+       return writeFileAsync("READme.md", md)
+
+    })
+    .then(() => {
+        console.log("success");
+
+    })
+    .catch((err) => {
+        console.log(err);
+
+    });
+
+
+
+
+
+
+
+
+
+
+// .then(function ({ username }) {
+//     const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+//       axios.get(queryUrl)
+//           .then((response) => {
+//               //   console.log(response.data);
+//               let avatar = response.data.avatar_url;
+//               let githubUrl = response.data.url             
+//               // return writeFileAsync("README.md", md);
+//           })
+//   })
+
+
